c31907d90651e9cdc3f9ab15b45cdce07f51d0f4
Peace Gusenga <37972802+PeaceGusenga@users.noreply.github.com>
2022-01-11 11:48:08 +0200

    Add files via upload

---
+++ Test Suites/Testing OnlineStore UI.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Testing the Demoblaze site</description>
+   <name>Testing OnlineStore UI</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c59f6e93-72a7-459e-83c5-2ad14dc861ff</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d3ed469-0509-4810-a5e2-39a2597d69e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Attempt Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eec8fcdb-f0a0-4b6b-ab51-62fb5f197e8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Attempt to SignUp</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b08a3ced-a250-4f8a-8e9f-cc7007b67bad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buy a Laptop</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7fc28b47-8811-4820-b40e-1be0d333d8e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buy a Monitor</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cf49794d-e63c-4b96-8b5a-588a824a1f21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buy a Phone</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f62cffb-735b-4ef6-b0e7-5be105231db9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Mail ContactUs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2bcbc603-9a76-4395-b7e3-e67d153fec40</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Play AboutUs Video</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
